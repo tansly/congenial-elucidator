faa561678a164173271474dcc6dd578b30f7af42
Yağmur Oymak <yagmur.oymak@gmail.com>
2018-12-20 21:03:03 +0300

    Fix indentation with vim

--- src/xpln.yalalr.lisp
+++ src/xpln.yalalr.lisp
@@ -41,8 +41,8 @@
 (defun mk-sym-entry (name)
   "NB: Lisp hash is collision-free, duplicates just replace the older value."
   (cond ((numberp name) (setf (gethash (list name *blockno*) *symtab*) (list 'num name)))
-	((symbolp name) (setf (gethash (list name *blockno*) *symtab*) (list 'var name)))
-	(t (setf (gethash (list name *blockno*) *symtab*) (list 'unknown name)))))
+        ((symbolp name) (setf (gethash (list name *blockno*) *symtab*) (list 'var name)))
+        (t (setf (gethash (list name *blockno*) *symtab*) (list 'unknown name)))))
 
 (defun sym-get-type (val)
   (first val))
@@ -91,8 +91,8 @@
   If you have more than one block, you need to create .data for each block."
   (format t "~2%.data~%")
   (maphash #'(lambda (key val)
-	       (if (equal (sym-get-type val) 'VAR) (format t "~%~A: .word 0" (sym-get-value val))))
-	   *symtab*))
+               (if (equal (sym-get-type val) 'VAR) (format t "~%~A: .word 0" (sym-get-value val))))
+           *symtab*))
 
 (defun mk-mips (p register)
   "create li if constant or lw if not"
@@ -105,9 +105,9 @@
 
 (defun mk-mips-3ac (i)
   (let ((op (tac-get-mips (first i)))
-	(p1 (second i))
-	(p2 (third i))
-	(p3 (fourth i)))
+        (p1 (second i))
+        (p2 (third i))
+        (p3 (fourth i)))
     (mk-mips p2 "$t0")
     (mk-mips p3 "$t1")
     (format t "~%~A $t0,$t0,$t1" op)
@@ -115,15 +115,15 @@
 
 (defun mk-mips-2ac (i)
   (let ((op (tac-get-mips (first i)))
-	(p1 (second i))
-	(p2 (third i)))
+        (p1 (second i))
+        (p2 (third i)))
     (mk-mips p2 "$t1")
     (format t "~%~A $t0,$zero,$t1" op)
     (format t "~%sw $t0,~A" p1)))
 
 (defun mk-mips-2copy (i)
   (let ((p1 (first i))
-	(p2 (second i)))
+        (p2 (second i)))
     (mk-mips p2 "$t0")
     (format t "~%sw $t0,~A" p1)))
 
@@ -134,9 +134,9 @@
   (dolist (instruction (second code)) ; NB. code is a grammar variable feature (code i1 i2 i3 ...)
     (let ((itype (first instruction)))
       (cond ((equal itype '3AC) (mk-mips-3ac (rest instruction)))
-	    ((equal itype '2AC) (mk-mips-2ac (rest instruction)))
-	    ((equal itype '2COPY) (mk-mips-2copy (rest instruction)))
-	    (t (format t "unknown TAC code: ~A" instruction))))))
+            ((equal itype '2AC) (mk-mips-2ac (rest instruction)))
+            ((equal itype '2COPY) (mk-mips-2copy (rest instruction)))
+            (t (format t "unknown TAC code: ~A" instruction))))))
 
 (defun tac-to-rac (code)
   (format t  "~2%TAC code:~2%")
@@ -173,71 +173,70 @@
 ;;;; LALR data 
 
 (defparameter grammar
-'(
-  (start --> ID COLON EQLS e END  #'(lambda (ID COLON EQLS e END) (tac-to-rac (mk-code (append (var-get-code e)
-												(mk-2copy (t-get-val ID)
-													  (var-get-place e)))))))
-  (e     --> e ADD te         #'(lambda (e ADD te) (let ((newplace (newtemp)))
-						     (mk-sym-entry newplace)
-						     (list (mk-place newplace)
-							   (mk-code (append (var-get-code e)
-									    (var-get-code te)
-									    (mk-3ac 'add newplace
-										    (var-get-place e)
-										    (var-get-place te))))))))
-  (e     --> e SUB te         #'(lambda (e SUB te) (let ((newplace (newtemp)))
-						     (mk-sym-entry newplace)
-						     (list (mk-place newplace)
-							   (mk-code (append (var-get-code e)
-									    (var-get-code te)
-									    (mk-3ac 'sub newplace
-										    (var-get-place e)
-										    (var-get-place te))))))))
-  (e     --> te               #'(lambda (te) (identity te)))
-  (te    --> te MULT f        #'(lambda (te MULT f) (let ((newplace (newtemp)))
-						     (mk-sym-entry newplace)
-						     (list (mk-place newplace)
-							   (mk-code (append (var-get-code te)
-									    (var-get-code f)
-									    (mk-3ac 'mult newplace
-										    (var-get-place te)
-										    (var-get-place f))))))))
-  (te    --> te DIV f         #'(lambda (te DIV f) (let ((newplace (newtemp)))
-						     (mk-sym-entry newplace)
-						     (list (mk-place newplace)
-							   (mk-code (append (var-get-code te)
-									    (var-get-code f)
-									    (mk-3ac 'div newplace
-										    (var-get-place te)
-										    (var-get-place f))))))))
-  (te    --> f                #'(lambda (f)(identity f)))
-  (f     --> LP e RP          #'(lambda (LP e RP) (identity e)))
-  (f     --> SUB ID           #'(lambda (SUB ID) (let ((newplace (newtemp)))
-						   (mk-sym-entry newplace)
-						   (list (mk-place newplace)
-							 (mk-code (mk-2ac 'uminus newplace
-										  (t-get-val ID)))))))
-  (f     --> ID               #'(lambda (ID) (progn 
-					       (mk-sym-entry (t-get-val ID))
-					       (list (mk-place (t-get-val ID))
-						     (mk-code nil)))))
-  ))
+  '(
+    (start --> ID COLON EQLS e END  #'(lambda (ID COLON EQLS e END) (tac-to-rac (mk-code (append (var-get-code e)
+                                                                                                 (mk-2copy (t-get-val ID)
+                                                                                                           (var-get-place e)))))))
+    (e     --> e ADD te         #'(lambda (e ADD te) (let ((newplace (newtemp)))
+                                                       (mk-sym-entry newplace)
+                                                       (list (mk-place newplace)
+                                                             (mk-code (append (var-get-code e)
+                                                                              (var-get-code te)
+                                                                              (mk-3ac 'add newplace
+                                                                                      (var-get-place e)
+                                                                                      (var-get-place te))))))))
+    (e     --> e SUB te         #'(lambda (e SUB te) (let ((newplace (newtemp)))
+                                                       (mk-sym-entry newplace)
+                                                       (list (mk-place newplace)
+                                                             (mk-code (append (var-get-code e)
+                                                                              (var-get-code te)
+                                                                              (mk-3ac 'sub newplace
+                                                                                      (var-get-place e)
+                                                                                      (var-get-place te))))))))
+    (e     --> te               #'(lambda (te) (identity te)))
+    (te    --> te MULT f        #'(lambda (te MULT f) (let ((newplace (newtemp)))
+                                                        (mk-sym-entry newplace)
+                                                        (list (mk-place newplace)
+                                                              (mk-code (append (var-get-code te)
+                                                                               (var-get-code f)
+                                                                               (mk-3ac 'mult newplace
+                                                                                       (var-get-place te)
+                                                                                       (var-get-place f))))))))
+    (te    --> te DIV f         #'(lambda (te DIV f) (let ((newplace (newtemp)))
+                                                       (mk-sym-entry newplace)
+                                                       (list (mk-place newplace)
+                                                             (mk-code (append (var-get-code te)
+                                                                              (var-get-code f)
+                                                                              (mk-3ac 'div newplace
+                                                                                      (var-get-place te)
+                                                                                      (var-get-place f))))))))
+    (te    --> f                #'(lambda (f)(identity f)))
+    (f     --> LP e RP          #'(lambda (LP e RP) (identity e)))
+    (f     --> SUB ID           #'(lambda (SUB ID) (let ((newplace (newtemp)))
+                                                     (mk-sym-entry newplace)
+                                                     (list (mk-place newplace)
+                                                           (mk-code (mk-2ac 'uminus newplace
+                                                                            (t-get-val ID)))))))
+    (f     --> ID               #'(lambda (ID) (progn
+                                                 (mk-sym-entry (t-get-val ID))
+                                                 (list (mk-place (t-get-val ID))
+                                                       (mk-code nil)))))
+    ))
 
 (defparameter lexforms '(ID END COLON EQLS LP RP ADD SUB MULT DIV))
 
 (defparameter lexicon '(
-	          (\; END) ;; all but ID goes in the lexicon
-		  (|:| COLON)
-		  (|=| EQLS)
-		  (|(| LP)
-		  (|)| RP)
-		  ($ $)        ; this is for lalrparser.lisp's end of input
-		  (+ ADD)
-		  (- SUB)
-		  (* MULT)
-		  (/ DIV)
-		  ))
-  ;; if you change the end-marker, change its hardcopy above in lexicon above as well.
-  ;; (because LALR parser does not evaluate its lexicon symbols---sorry.)
+                        (\; END) ;; all but ID goes in the lexicon
+                        (|:| COLON)
+                        (|=| EQLS)
+                        (|(| LP)
+                          (|)| RP)
+                        ($ $)        ; this is for lalrparser.lisp's end of input
+                        (+ ADD)
+                        (- SUB)
+                        (* MULT)
+                        (/ DIV)
+                        ))
+;; if you change the end-marker, change its hardcopy above in lexicon above as well.
+;; (because LALR parser does not evaluate its lexicon symbols---sorry.)
 (defparameter *ENDMARKER* '$)
-
